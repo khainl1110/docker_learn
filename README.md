f7f4a8fab9f56dd00908f77929b0b778f10e0dd7
Khai Nguyen <khainl1110@gmail.com>
2021-09-06 15:51:11 -0700

    Working on dev images

---
+++ README.md
@@ -0,0 +1,8 @@
+# Docker
+- Run: docker run [image name]
+    + This will create new docker image instance everytime, need to refer by its tag name
+    + dock start [tag name]
+- Build: docker build [directory]
+    + add -t to tag the image
+- Run using dockerfile.dev: docker build -f Dockerfile.dev .
+- Want to ignore some files? Use .dockerignore
--- server/index.js
+++ server/index.js
@@ -1,64 +1,27 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql2');
+const redis = require('redis');
 
-// database connection
-const db = mysql.createPool({
-  host: 'mysql_db',
-  user: 'MYSQL_USER',
-  password: 'MYSQL_PASSWORD',
-  database: 'books'
-})
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const app = express();
+const client = redis.createClient({
+    host: 'redis-server'
+    });
+client.set('visits', 0);
 
 // App
 const app = express();
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send({'message':'Hello World'});
-});
+app.get("/", (req, res) => {
+    client.get('visits', (err, visits) => {
+        res.send({"Message": 'Number of visits is ' + visits});
+        client.set('visits', parseInt(visits) +1);
+    })
 
-// get all the books in the database
-app.get('/get', (req, res) => {
-  const SelectQuery = "select * from books_reviews"
-  db.query(SelectQuery, (err, result) => {
-    res.send(result)
-  })
 })
 
-// add a book to the database
-app.post('/insert', (req, res) => {
-  const bookName = req.body.bookName;
-  const bookReview = req.body.review;
-  const InsertQuery = "insert into books_reviews (book_name, book_review) values (?, ?)"
-  db.query(InsertQuery, [bookName, bookReview], (err, result) => {
-    console.log(result)
-  })
+let port = 8080
+app.listen(port , () => {
+    console.log(`Listening to port: ${port}`)
 })
-
-// delete a book from the database
-app.delete("/delete/:bookId", (req, res) => {
-  const bookId = req.params.bookId;
-  const DeleteQuery = "DELETE FROM books_reviews WHERE id = ?";
-  db.query(DeleteQuery, bookId, (err, result) => {
-    if (err) console.log(err);
-  })
-})
-
-// update a book review
-app.put("/update/:bookId", (req, res) => {
-  const bookReview = req.body.reviewUpdate;
-  const bookId = req.params.bookId;
-  const UpdateQuery = "UPDATE books_reviews SET book_review = ? WHERE id = ?";
-  db.query(UpdateQuery, [bookReview, bookId], (err, result) => {
-    if (err) console.log(err)
-  })
-})
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
